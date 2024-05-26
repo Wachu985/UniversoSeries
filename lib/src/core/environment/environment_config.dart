7654db554a77347299c267ce745dbe02ea1631e6
Wachu985 <pedrobonilla985@gmail.com>
2024-05-26 01:14:02 -0400

    chore: added keys for google auth

--- lib/src/core/environment/environment_config.dart
+++ lib/src/core/environment/environment_config.dart
@@ -3,4 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class EnvironmentConfig {
   static final String apiUrl = dotenv.get("API_URL");
   static final String apiKey = dotenv.get("API_KEY");
+  static final String webClientId = dotenv.get("WEB_CLIENT_ID");
+  static final String androidClientId = dotenv.get("ANDROID_CLIENT_ID");
 }
